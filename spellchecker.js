6fa27ecf7c424efbc5352dd2fb0f685e90bff768
leithlangley <gigim14@gmail.com>
2020-11-18 13:09:35 +0400

    update

--- spellchecker.js
+++ spellchecker.js
@@ -1,57 +1,32 @@
-let words = ['mgeli', 'tura', 'mela']
+let words = [
+  'word1',
+  'word2',
+  'word3',
+  'word4',
+  'word5',
+  'word6',
+  'word7',
+  'word8',
+  'word9',
+  'word10',
+]
 CKEDITOR.replace('spellcheck')
 CKEDITOR.on('instanceReady', (e) => {
   e.editor.on('key', (e) => {
-    if (e.data.keyCode == 32) {
-      let text = CKEDITOR.instances.spellcheck.editable().getText()
+    if (e.data.keyCode == 32 || e.data.keyCode == 13) {
+      let text = e.editor.editable().getText()
       spellCheck(text)
       //for update caret position after set
-      var range = CKEDITOR.instances.spellcheck.createRange()
-      range.moveToElementEditablePosition(
-        CKEDITOR.instances.spellcheck.editable(),
-        true
-      )
-      CKEDITOR.instances.spellcheck.getSelection().selectRanges([range])
+      let range = e.editor.createRange()
+      range.moveToElementEditablePosition(e.editor.editable(), true)
+      e.editor.getSelection().selectRanges([range])
       //for update caret position after set
     }
   })
 })
-
 const spellCheck = (text) => {
-  // let e = CKEDITOR.instances.spellcheck.editable().getText()
-  // words.forEach((word) => {
-  //   let res = new RegExp('(?:^|\\s)' + word, 'gi')
-  //   let test = text.replaceAll(res, ` <u style="color:green">${word}</u>`)
-  //   CKEDITOR.instances.spellcheck.editable().setData(test, { internal: true })
-  //   // let texts = CKEDITOR.instances.spellcheck.editable().getData()
-  //   console.log(text)
-  // })
-
-  //
-  let e = CKEDITOR.instances.spellcheck.editable().getData()
-  words.forEach((word) => {
-    let res = new RegExp('(?:^|\\s)' + word, 'gi')
-    let test = text.replaceAll(res, ` <u style="color:green">${word}</u>`)
-
-    let splitted = text.split(' ')
-    splitted.forEach((i) => {
-      if (word == i) {
-        let res = new RegExp('(?:^|\\s)' + word, 'gi')
-        let test = text.replace(res, ` <u style="color:green">${word}</u>`)
-        CKEDITOR.instances.spellcheck.editable().setData(test, (e) => {
-          // console.log(e)
-        })
-      }
-    })
-  })
-  console.log(e)
+  w = words.join('|')
+  let reg = new RegExp('(?:^|\\s)' + `${w}`, 'gi')
+  text = text.replace(reg, (word) => ` <u style="color:green">${word}</u>`)
+  CKEDITOR.instances.spellcheck.editable().setData(text, () => {})
 }
-// const underline = (test) => {
-//   // console.log(test)
-//   // let text = CKEDITOR.instances.spellcheck.editable().getText()
-//   // if (word) {
-//   //   // console.log(text)
-//   //   let replace = text.replaceAll(word, `<u style='color:green'>${word}</u>`)
-//   //   CKEDITOR.instances.spellcheck.editable().setData(replace, () => {})
-//   // }
-// }
